358d38c6afd917781b2bf621039b8149244703ab
Ben Long <14benj@gmail.com>
2017-11-16 14:43:55 -0500

    Updating component class names to be more specific for the component

--- src/LgsAlert.vue
+++ src/LgsAlert.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="lgs-alert" :class="{flyin: flyin}" v-if="show" role="alert">
+  <div class="lgs-alert" :class="{lgs-flyin: flyin}" v-if="show" role="alert">
     <slot name="header"></slot>
     <slot name="body"></slot>
     <slot name="dismiss">
-      <a class="close" href="javascript:;" v-on:click="onDismiss()">
-        <svg class="btn-close" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+      <a class="lgs-close" href="javascript:;" v-on:click="onDismiss()">
+        <svg class="lgs-btn-close" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
       </a>
     </slot>
   </div>
@@ -43,7 +43,7 @@
     position: relative;
   }
 
-  .lgs-alert .close {
+  .lgs-alert .lgs-close {
     position: absolute;
     top: 0px;
     right: 0px;
@@ -51,13 +51,13 @@
     margin-top: 2em;
   }
 
-  .lgs-alert .btn-close {
+  .lgs-alert .lgs-btn-close {
     color: #000000;
     height: 20px;
     width: 20px;
   }
 
-  .flyin {
+  .lgs-flyin {
     position: fixed;
     top: 50px;
     right: -10000px;
