c9148cdd2c573d52999fe3ef983d79f54a6984f4
Ben Long <14benj@gmail.com>
2017-11-16 09:44:33 -0500

    Removing message prop in favor of empty slots

--- src/LgsAlert.vue
+++ src/LgsAlert.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="lgs-alert" :class="{flyin: flyin}" v-if="active" role="alert">
-    <slot name="body">
-      <h3 v-if="message.title">{{ message.title }}</h3>
-      <p>{{ message.body }}</p>
-    </slot>
+  <div class="lgs-alert" :class="{flyin: flyin}" v-if="show" role="alert">
+    <slot name="header"></slot>
+    <slot name="body"></slot>
     <slot name="dismiss">
-      <a class="close" href="javascript:;" v-on:click="onClick()">
+      <a class="close" href="javascript:;" v-on:click="onDismiss()">
         <svg class="btn-close" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
       </a>
     </slot>
@@ -17,24 +15,22 @@
     name: 'LgsAlert',
     props: {
       'active': {
-        default: false,
+        default: true,
         type: Boolean
       },
       'flyin': {
         default: false,
         type: Boolean
       },
-      'message': {
-        default: {
-          title: '',
-          body: ''
-        },
-        type: Object
+    },
+    data: function () {
+      return {
+        show: this.active
       }
     },
     methods: {
-      onClick: function () {
-        this.active = false
+      onDismiss: function () {
+        this.show = false
       }
     }
   }
